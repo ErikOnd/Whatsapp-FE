9e6ee19272534de946b89018dbf0e903fb9300a0
ajeer333 <ajeer333@gmail.com>
2023-04-17 22:27:14 +0200

    Update Registration.tsx

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,6 +1,6 @@
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import "../css/reg.css";
-//import GoogleButton from "react-google-button";
+import GoogleButton from "react-google-button";
 import regImage from "../assets/Registration_image.png";
 import { useState } from "react";
 
@@ -52,7 +52,7 @@ const Registration = () => {
               <p className="text-muted text-center">Getting started is easy</p>
               <div className="d-flex justify-content-center align-items-center ml-auto">
                 {/* <div className=" d-flex " style={{ gap: "10px" }}> */}
-                {/* <GoogleButton type="light" /> */}
+                <GoogleButton type="light" />
                 {/* <Button>Facebook</Button> */}
                 {/* </div> */}
               </div>
@@ -134,14 +134,15 @@ const Registration = () => {
                         onChange={uploadUserPicture}
                       />
                     </Row>
-
-                    <Button
-                      variant="success"
-                      type="submit"
-                      className="btnSubmit mt-5 "
-                    >
-                      Create Account
-                    </Button>
+                    <div className="d-flex justify-content-center align-items-center">
+                      <Button
+                        variant="success"
+                        type="submit"
+                        className="btnSubmit mt-5 "
+                      >
+                        Create Account
+                      </Button>
+                    </div>
                   </div>
                 </Form>
               </div>
