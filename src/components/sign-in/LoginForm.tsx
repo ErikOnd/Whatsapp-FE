00ce9b1155ff728823f801a78203822af81333f4
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-04-18 16:21:48 +0200

    login functionality

--- src/components/sign-in/LoginForm.tsx
+++ src/components/sign-in/LoginForm.tsx
@@ -1,22 +1,69 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
 // import { useDispatch } from "react-redux";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { IUser } from "../../interfaces/IUser";
+
 const LoginForm = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    e.preventDefault();
+    try {
+      const apiUrl = process.env.REACT_APP_BE_URL;
+      const response = await fetch(`${apiUrl}/users/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        toast("Login successfull! 💪", { autoClose: 1000 });
+        const data = await response.json();
+        console.log("logindata", data);
+        // console.log("accessToken", data.accessToken);
+        localStorage.setItem("accessToken", data.accessToken);
+        // localStorage.setItem("refreshToken", data.refreshToken);
+        navigate("/home");
+        // window.location.href = "/home";
+      } else {
+        console.log("Login failed");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className=" my-3 mx-2">
+    <Form className=" my-3 mx-2 ">
       <div className="mb-2">
         <Form.Group className="mb-4">
-          <Form.Control type="email" placeholder="Email" />
+          <Form.Control
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
         <Form.Group>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
       </div>
       <div>
-        <Button className="mt-2 w-100 ">Log In</Button>
+        <Button type="submit" className="mt-2 w-100 " onClick={handleSubmit}>
+          Log In
+        </Button>
       </div>
-    </div>
+    </Form>
   );
 };
 export default LoginForm;
